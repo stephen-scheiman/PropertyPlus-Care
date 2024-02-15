209ea8b1e9224721d46699ab9416fa3067f22fe2
Meow Roar <sareacct91@gmail.com>
2024-02-15 13:21:21 -0800

    added morgan loggig

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.use(session(sess));
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
+app.use(morgan('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
