323cf384129278214e1253ec69cbcc29da8d00ef
Meow Roar <sareacct91@gmail.com>
2024-02-16 13:01:08 -0800

    fix reorder the seeding and added VendorIssueData

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,12 @@
 const sequelize = require('../config/connection');
-const { User,
+const {
+  User,
   Owner,
   Property,
   Issue,
   Task,
   Vendor,
+  VendorIssue
 } = require('../models');
 
 const userData = require('./userData.json');
@@ -13,6 +15,7 @@ const ownerData = require('./ownerData.json');
 const propertyData = require('./propertyData.json');
 const taskData = require('./taskData.json');
 const vendorData = require('./vendorData.json');
+const vendorIssueData = require('./vendorIssueData.json')
 
 
 const seedDatabase = async () => {
@@ -23,11 +26,35 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  await Issue.bulkCreate(issueData, {});
-  await Owner.bulkCreate(ownerData, {});
-  await Property.bulkCreate(propertyData, {});
-  await Task.bulkCreate(taskData, {});
-  await Vendor.bulkCreate(vendorData, {});
+  await Vendor.bulkCreate(vendorData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Owner.bulkCreate(ownerData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Property.bulkCreate(propertyData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Issue.bulkCreate(issueData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Task.bulkCreate(taskData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await VendorIssue.bulkCreate(vendorIssueData, {
+    individualHooks: true,
+    returning: true,
+  })
 
   process.exit(0);
 };
