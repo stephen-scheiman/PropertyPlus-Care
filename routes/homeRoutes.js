dfcf6c9d83e4771c9dd8e62d6999b212296bed76
Meow Roar <sareacct91@gmail.com>
2024-02-16 16:59:21 -0800

    moved testing routes off of homeRoutes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -5,6 +5,5 @@ const withAuth = require('../utils/auth.js');
 router.get('/', c.renderHome);
 // router.get('/', withAuth, c.renderHome);
 router.get('/login', c.userLogin);
-router.get('/test', (req, res) => {res.render("homepage")});
 
 module.exports = router;
