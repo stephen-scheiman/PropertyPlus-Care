6ae19770912a10adad774c705c69620c10f01e92
Sarah Chambers <chamberssarahann@gmail.com>
2024-02-16 17:47:11 -0800

    added testing routes for icon paths

--- routes/test/testRoutes.js
+++ routes/test/testRoutes.js
@@ -3,5 +3,16 @@ const router = require('express').Router();
 router.route('/')
   .get((req, res) => {res.render("homepage")})
 
+router.route("/pg1")
+  .get((req, res) => {res.render("test1", {layout:false})})
+
+router.route("/pg2")
+  .get((req, res) => {res.render("test2", {layout:false})})
+
+router.route("/pg3")
+  .get((req, res) => {res.render("test3", {layout:false})})
+
+router.route("/pg4")
+  .get((req, res) => {res.render("test4", {layout:false})})
 
 module.exports = router;
