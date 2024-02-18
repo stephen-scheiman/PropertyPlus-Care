601184e1cdf29979247063ca37d71eb66cbd158d
Stephen Scheiman <147669992+stephen-scheiman@users.noreply.github.com>
2024-02-18 12:42:28 -0800

    Get all tasks and render function complete

--- controller/api/taskController.js
+++ controller/api/taskController.js
@@ -1,4 +1,40 @@
-const { Task } = require('../../models');
+const { Property, Issue, Task } = require('../../models');
+const { BadRequestError, InternalServerError } = require('../../utils/errors');
+
+/* for purposes of unit testing, separating sequelize request function
+from render data functions */
+// sequelize get all task and related table data
+async function getAllTasks() {
+    const taskData = Task.findAll({
+      include: [{
+        model: Property,
+        attributes: ["property_name"],
+        model: Issue,
+        attributes: ["issue_title"],
+      }],
+      raw: true,
+      nest: true
+    });
+  
+    if (!taskData) {
+      throw new BadRequestError('Something went wrong');
+    }
+  
+    return taskData;
+  };
+
+// render task data function
+async function renderTasks(req, res) {
+    const tasks = await getAllTasks();  
+    res.status(200).json({ tasks });
+  };
+
+
+
+
+
+
+
 
 
 // module.exports = // { function names } ;
