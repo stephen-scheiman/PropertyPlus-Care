2c32cddf5a8cd03a7b5ee82ed05442205cd816f8
Meow Roar <sareacct91@gmail.com>
2024-02-18 13:23:44 -0800

    redirect to testing pages

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const testRoutes = require('./test/testRoutes');
 
+router.get('/', (req, res) => { res.status(200).set('hx-Redirect', '/test').redirect('/test')});
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/test', testRoutes);
