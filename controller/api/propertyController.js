cf42f3b9ede51b9baeea40029957437079654c74
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-02-17 18:27:09 -0800

    completed createProperty function

--- controller/api/propertyController.js
+++ controller/api/propertyController.js
@@ -1,6 +1,8 @@
 const { Property, Owner, Issue } = require('../../models');
-const { BadRequestError } = require('../../utils/errors');
+const { BadRequestError, InternalServerError } = require('../../utils/errors');
 
+/* for purposes of unit testing, separating sequelize request function
+from render data functions */
 async function getAllProperties() {
   const propertyData = Property.findAll({
     include: [{
@@ -18,11 +20,13 @@ async function getAllProperties() {
   return propertyData;
 };
 
+// render data function
 async function renderProperties(req, res) {
   const properties = await getAllProperties();  
   res.status(200).json({ properties });
 };
 
+// sequelize request function
 async function getPropertyByID(id) {
   const propertyData = Property.findByPk(id, {
     include: [
@@ -42,6 +46,7 @@ async function getPropertyByID(id) {
   return propertyData;
 };
 
+// render data funciton
 async function renderOneProperty(req, res) {
   const { id: property_id } = req.params;
   const properties = await getPropertyByID(property_id);  
@@ -49,7 +54,32 @@ async function renderOneProperty(req, res) {
 };
 
 async function createProperty(req, res) {
+  const { property_name, property_street, property_city, property_state, property_zip, owner_id } = req.body;
+  // for owner_id, in front end, we need to present user with a list of potential owners to associate
+  // with this property, then transfer the id of the selected owner into here
+  // this means that before someone can add a new property, they have to first add the new Owner so...
+  if (!owner_id) {
+    throw new BadRequestError("You must select an existing property owner.");
+  };
 
+  if (!(property_name && property_street && property_city && property_state && property_zip && owner_id)) {
+    throw new BadRequestError("Missing Data - Please complete all fields");
+  }
+
+  const newProperty = await Property.create({
+    property_name,
+    property_street,
+    property_city,
+    property_state,
+    property_zip,
+    owner_id
+  });
+
+  if (!newProperty) {
+    throw new InternalServerError("New Property creation failed.");
+  } else {
+    res.status(200).json({ msg: "New Property succesfully created!" });
+  };
 };
 
 async function updateProperty(req, res) {
