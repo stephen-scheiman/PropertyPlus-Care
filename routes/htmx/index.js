57961a7836ad1ed1538db971fe2d439fa38788dc
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-02-19 15:32:06 -0800

    issue-main.hbs mostly complete, routes in place

--- routes/htmx/index.js
+++ routes/htmx/index.js
@@ -3,10 +3,12 @@ const c = require('../../controller/homeController.js');
 const withAuth = require('../../utils/auth.js');
 
 const propertiesRoutes = require('./propertiesHxRoutes.js');
+const issueHxRoutes = require('./issueHxRoutes.js');
 
 router.get('/', c.renderHome);
 // router.get('/', withAuth, c.renderHome);
 router.get('/login', c.userLogin);
 router.use('/properties', propertiesRoutes);
+router.use('/issues', issueHxRoutes);
 
 module.exports = router;
