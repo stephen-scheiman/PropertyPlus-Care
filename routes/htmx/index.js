f32246dbe814b74313d0892d005ea1429a7e3ae3
Meow Roar <sareacct91@gmail.com>
2024-02-18 21:13:16 -0800

    Update propertyHxRoutes

--- routes/htmx/index.js
+++ routes/htmx/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const c = require('../../controller/homeController.js');
 const withAuth = require('../../utils/auth.js');
 
-const propertiesRoutes = require('./propertiesHxRoutes.js');
+const propertiesRoutes = require('./propertyHxRoutes.js');
 
 router.get('/', c.renderHome);
 // router.get('/', withAuth, c.renderHome);
