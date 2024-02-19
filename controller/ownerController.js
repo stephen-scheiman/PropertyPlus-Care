54a009992a8eee826984e0f1937b3829eb8c4468
Stephen Scheiman <147669992+stephen-scheiman@users.noreply.github.com>
2024-02-19 09:59:58 -0800

    owner controller input validation and input reformatting

--- controller/ownerController.js
+++ controller/ownerController.js
@@ -17,7 +17,7 @@ async function renderOneOwner(req, res) {
 
 async function renderNewOwner(req, res) {
   console.log(req.body);
-  const {
+  let { //changed to let to allow for automated corrections
     owner_first_name,
     owner_last_name,
     owner_email,
@@ -27,6 +27,32 @@ async function renderNewOwner(req, res) {
     owner_state,
     owner_zip
   } = req.body;
+
+  //validate letters only
+  const namePattern = /^[a-zA-Z]+$/;
+  //validate properly formed email
+  const emailPattern = /^ [a-zA-Z0-9._-]+@ [a-zA-Z0-9.-]+\\. [a-zA-Z] {2,6}$/;
+  //validate properly formed phone number
+  const phonePattern = /^(?:(\d{3}))?[- ]?(\d{3})[- ]?(\d{4,6})$/im;
+  //validate proper street address
+  const streetPattern = /^[a-zA-Z0-9\s,'-]*$/;
+  //validate two letter state
+  const statePattern = /^[a-zA-Z]{2}$/;
+  //validate zip is 5 digit number
+  const zipPattern = /^\d{5}$/;
+
+  //format name before sending to db
+  owner_first_name = owner_first_name[0].toUpperCase() + owner_first_name.slice(1).toLowerCase();
+  console.log(owner_first_name);
+  owner_last_name = owner_last_name[0].toUpperCase() + owner_last_name.slice(1).toLowerCase();
+  console.log(owner_last_name);
+  //format the phone number as (XXX)XXX-XXXX
+  owner_phone = owner_phone.replace(/[^a-zA-Z0-9 ]/g, '');
+  owner_phone = "(" + owner_phone.slice(0,3) + ")" + owner_phone.slice(3,6) + "-" + owner_phone.slice(6);
+  console.log(owner_phone);
+
+
+
 
   if (!(owner_first_name && owner_last_name && owner_email && owner_phone && owner_street && owner_city && owner_state && owner_zip)) {
     throw new BadRequestError('Missing data, please fill out all forms');
