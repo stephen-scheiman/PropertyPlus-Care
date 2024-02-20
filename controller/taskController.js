f7b07ce4082ba2e522e0bcc267edff2c9ab525f5
stephen scheiman <sbscheiman@gmail.com>
2024-02-20 08:14:24 -0800

    Tasks followUp_date validation complete and test

--- controller/taskController.js
+++ controller/taskController.js
@@ -2,8 +2,9 @@ const { Task } = require("../models");
 const { BadRequestError, InternalServerError } = require("../utils/errors");
 const {
   getAllTasks,
-  getTaskByID, 
-  updateIsDone } = require("../utils/queries/tasks");
+  getTaskByID,
+  updateIsDone,
+} = require("../utils/queries/tasks");
 
 async function renderTasks(req, res) {
   const tasks = await getAllTasks();
@@ -24,14 +25,14 @@ async function renderIsDone(req, res) {
     task = await updateIsDone(id, false);
   } else {
     task = await updateIsDone(id, true);
-  };
+  }
 
   res.status(200).render("issue-ID", { task });
 }
 
 // create task function
 async function createTask(req, res) {
-  const { task_name, status_update, followUp_date, property_id, issue_id } =
+  let { task_name, status_update, followUp_date, property_id, issue_id } =
     req.body;
 
   if (
@@ -40,11 +41,13 @@ async function createTask(req, res) {
     throw new BadRequestError("Missing Data - Please complete all fields");
   }
 
-  let dateObj = new Date(followUp_date);
-
-  if (isNaN(dateObj)) {
-      throw new BadRequestError("Please enter a valid date");
-  } 
+  followUp_date = new Date(followUp_date);
+ 
+  if (isNaN(followUp_date)) {
+    throw new BadRequestError(
+      "Please enter a valid date in the form of MM/DD/YY",
+    );
+  }
 
   const newTask = await Task.create({
     task_name,
@@ -64,13 +67,13 @@ async function createTask(req, res) {
 //update task function
 async function updateTask(req, res) {
   const task_id = req.params.id;
-  const { task_name, status_update, followUp_date, is_done } = req.body;
+  let { task_name, status_update, followUp_date, is_done } = req.body;
 
-  let dateObj = new Date(followUp_date);
-
-  if (isNaN(dateObj)) {
-      throw new BadRequestError("Please enter a valid date");
-  } 
+  followUp_date = new Date(followUp_date);
+ 
+  if (isNaN(followUp_date)) {
+    throw new BadRequestError("Please enter a valid date in the form MM/DD/YY");
+  }
 
   const taskData = await Task.update(
     {
@@ -93,4 +96,10 @@ async function updateTask(req, res) {
   }
 }
 
-module.exports = { renderTasks, renderOneTask, createTask, updateTask, renderIsDone };
+module.exports = {
+  renderTasks,
+  renderOneTask,
+  createTask,
+  updateTask,
+  renderIsDone,
+};
