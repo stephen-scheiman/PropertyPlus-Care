c15ba6877052b9bf0eed5c3a48c2f15a91fbbb21
Meow Roar <sareacct91@gmail.com>
2024-02-19 17:56:21 -0800

    added queries utils

---
+++ utils/queries/issues.js
@@ -0,0 +1,92 @@
+const { Vendor, Issue, Property, Task } = require('../../models');
+
+async function findAllIIssues() {
+  const issues = await Issue.findAll({
+    include: [
+      { model: Property },
+      { model: Task}
+    ],
+    raw: true,
+    nest: true,
+  });
+
+  if (!issues) {
+    throw new NotFoundError('No issues found');
+  }
+  // console.log(issues);
+  return issues;
+};
+
+async function findOneIssue(issue_id) {
+  const issue = await Issue.findByPk(issue_id, {
+    include: [
+      { model: Property, },
+      { model: Task, },
+      { model: Vendor, },
+    ],
+    raw: true,
+    nest: true,
+  });
+
+  if (!issue) {
+    throw new NotFoundError(`No issue found wtih id ${issue_id}`);
+  }
+  // console.log(issue);
+  return issue;
+};
+
+async function createIssue(issueData) {
+  const issue = await Issue.create(issueData);
+
+  if (!issue) {
+    throw new InternalServerError("Couldn't create an issue");
+  }
+  // console.log(issue);
+  return issue.toJSON();
+};
+
+async function updateIssue(issue_id, issueData) {
+  const issue = await Issue.update(issueData, {
+    where: { issue_id }
+  });
+
+  if (!issue) {
+    throw new BadRequestError(`Couldn't update issue with id ${issue_id}`);
+  }
+  // console.log(issue);
+  return issue;
+};
+
+async function deleteIssue(issue_id) {
+  const issue = await Issue.destroy({ where: { issue_id } });
+
+  if (!issue) {
+    throw new BadRequestError(`Couldn't delete issue with id ${issue_id}`);
+  }
+  // console.log(issue);
+  return issue;
+};
+
+async function addVendorToIssue(issue_id, vendor_id) {
+  const issue = await Issue.findByPk(issue_id);
+
+  if (!issue) {
+    throw new BadRequestError(`No issue found with id ${issue_id}`);
+  }
+  
+  const result = await issue.addVendor(vendor_id);
+
+  // console.log(result);
+  return result;
+};
+
+
+module.exports = {
+  findAllIIssues,
+  findOneIssue,
+  createIssue,
+  updateIssue,
+  deleteIssue,
+  addVendorToIssue,
+
+}
---
+++ utils/queries/owners.js
@@ -0,0 +1,71 @@
+const { Owner, Property } = require("../../models");
+
+async function findOwners() {
+  const owners = await Owner.findAll({
+    raw: true,
+    nest: true,
+  });
+
+  if (!owners) {
+    throw new NotFoundError("No owners found");
+  }
+  // console.log(owners);
+  return owners;
+};
+
+async function findOwnerById(id) {
+  const owner = await Owner.findByPk(id, {
+    include: [
+      {
+        model: Property,
+      },
+    ],
+  });
+
+  if (!owner) {
+    throw new BadRequestError("No owner found");
+  }
+  // console.log(owner);
+  return owner.toJSON();
+};
+
+async function createOwner(newOwnerData) {
+  const owner = await Owner.create(newOwnerData);
+
+  if (!owner) {
+    throw new InternalServerError("Error creating new owner");
+  }
+  // console.log(owner);
+  return owner.toJSON();
+};
+
+async function updateOwner(owner_id, ownerData) {
+  const owner = await Owner.update(ownerData, {
+    where: { owner_id },
+  });
+
+  if (!owner) {
+    throw new InternalServerError("Couldn't update owner information");
+  }
+  // console.log(owner);
+  return owner;
+};
+
+async function deleteOwner(owner_id) {
+  const owner = await Owner.destroy({ where: { owner_id } });
+
+  if (!owner) {
+    throw new InternalServerError(`Couldn't delete owner with id ${owner_id}`);
+  }
+  // console.log(owner);
+  return owner;
+};
+
+module.exports = {
+  findOwnerById,
+  findOwners,
+  createOwner,
+  updateOwner,
+  deleteOwner,
+
+}
