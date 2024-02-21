27f168dcc4ae862c09a5ac3daf540710ac4528c5
Sarah Chambers <chamberssarahann@gmail.com>
2024-02-20 17:28:01 -0800

    updated /login route

---
+++ routes/htmx/loginHxRoutes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const c = require("../../controller/userController");
+
+router.route("/")
+  .get(c.renderLoginForm)
+  .post(c.renderLoggedInHome)
+
+
+
+module.exports = router;
