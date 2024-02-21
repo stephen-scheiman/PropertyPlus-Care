15f01998fc9b32dff29248587bac1c472852342e
Sarah Chambers <chamberssarahann@gmail.com>
2024-02-20 17:27:40 -0800

    logged out error to terminal

--- utils/error-handler.js
+++ utils/error-handler.js
@@ -1,4 +1,5 @@
 async function errorHandler(err, req, res, next) {
+  console.log(err);
   const customError = {
     message: err.msg || 'Something went wrong. Try again later.',
     statusCode: err.statusCode || 500
