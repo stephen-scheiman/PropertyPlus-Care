46426247edc7e459e3f648e3a812ba4662efb742
Sarah Chambers <chamberssarahann@gmail.com>
2024-02-20 17:27:18 -0800

    updated /login route

--- routes/htmx/index.js
+++ routes/htmx/index.js
@@ -2,13 +2,14 @@ const router = require('express').Router();
 const c = require('../../controller/homeController.js');
 const withAuth = require('../../utils/auth.js');
 
+const loginHxRoutes = require("./loginHxRoutes.js");
 const propertiesRoutes = require('./propertiesHxRoutes.js');
 const issueHxRoutes = require('./issueHxRoutes.js');
 const taskHxRoutes = require('./taskHxRoutes.js');
 
 router.get('/', c.renderHome);
 // router.get('/', withAuth, c.renderHome);
-router.get('/login', c.userLogin);
+router.use('/login', loginHxRoutes);
 router.use('/properties', propertiesRoutes);
 router.use('/issues', issueHxRoutes);
 router.use('/tasks', taskHxRoutes);
