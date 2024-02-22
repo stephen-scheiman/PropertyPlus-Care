b3ce713f7b567248a268990d600da1f543130698
Sarah Chambers <chamberssarahann@gmail.com>
2024-02-21 16:31:32 -0800

    Updated properties route

--- routes/htmx/index.js
+++ routes/htmx/index.js
@@ -3,14 +3,14 @@ const c = require('../../controller/homeController.js');
 const withAuth = require('../../utils/auth.js');
 
 const loginHxRoutes = require("./loginHxRoutes.js");
-const propertiesRoutes = require('./propertiesHxRoutes.js');
+const propertyHxRoutes = require('./propertyHxRoutes.js');
 const issueHxRoutes = require('./issueHxRoutes.js');
 const taskHxRoutes = require('./taskHxRoutes.js');
 
 router.get('/', c.renderHome);
 // router.get('/', withAuth, c.renderHome);
 router.use('/login', loginHxRoutes);
-router.use('/properties', propertiesRoutes);
+router.use('/properties', propertyHxRoutes);
 router.use('/issues', issueHxRoutes);
 router.use('/tasks', taskHxRoutes);
 
