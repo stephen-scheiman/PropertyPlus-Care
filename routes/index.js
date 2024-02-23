702eb9afb8ea5245a8b561667cac82dccae074a8
Meow Roar <sareacct91@gmail.com>
2024-02-22 18:07:49 -0800

    removed test files

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,8 @@ const router = require('express').Router();
 
 // const apiRoutes = require('./api');
 const hxRoutes = require('./htmx');
-const testRoutes = require('./test/testRoutes');
 
 router.use('/', hxRoutes);
 // router.use('/api', apiRoutes);
-// router.use('/test', testRoutes);
 
 module.exports = router;
