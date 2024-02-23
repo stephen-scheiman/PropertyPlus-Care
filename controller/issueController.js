c90da24f528632984ba9145087ec7d914f75c0eb
stephen scheiman <sbscheiman@gmail.com>
2024-02-23 06:34:06 -0800

    Added task post route

--- controller/issueController.js
+++ controller/issueController.js
@@ -1,58 +1,70 @@
-
-const { Vendor, Issue, Property, Task } = require('../models');
-const { NotFoundError, InternalServerError, BadRequestError } = require('../utils/errors');
-const { findTasksByIssueID, updateIsDone } = require('../utils/queries/tasks');
-const { createIssue, findOneIssue, findAllIssues, findOpenIssues, findClosedIssues, getIssuesByPropertyID, updateIssueDone, deleteIssue } = require('../utils/queries/issues');
+const { Vendor, Issue, Property, Task } = require("../models");
+const {
+  NotFoundError,
+  InternalServerError,
+  BadRequestError,
+} = require("../utils/errors");
+const { findTasksByIssueID, updateIsDone } = require("../utils/queries/tasks");
+const {
+  createIssue,
+  findOneIssue,
+  findAllIssues,
+  findOpenIssues,
+  findClosedIssues,
+  getIssuesByPropertyID,
+  updateIssueDone,
+  deleteIssue,
+} = require("../utils/queries/issues");
 
 async function renderOpenIssues(req, res) {
   const issues = await findOpenIssues();
   // console.log(issues);
-  res.status(200).render('issue-aside', { issues });
-};
+  res.status(200).render("issue-aside", { issues });
+}
 
 async function renderIssues(req, res) {
   const { issueStatus } = req.query;
 
-  console.log('-----------------');
-  console.log('\n\n', issueStatus, '\n\n');
-  console.log('-----------------');
+  console.log("-----------------");
+  console.log("\n\n", issueStatus, "\n\n");
+  console.log("-----------------");
 
   switch (issueStatus) {
-    case 'open': {
+    case "open": {
       const issues = await findOpenIssues();
-      return res.status(200).render('issue-aside', { issues, layout: false });
+      return res.status(200).render("issue-aside", { issues, layout: false });
     }
 
-    case 'closed': {
+    case "closed": {
       const issues = await findClosedIssues();
-      return res.status(200).render('issue-aside', { issues, layout: false });
+      return res.status(200).render("issue-aside", { issues, layout: false });
     }
 
-    case 'all': {
+    case "all": {
       const issues = await findAllIssues();
       // console.log(issues);
-      res.status(200).render('issue-aside', { issues, layout: false });
+      res.status(200).render("issue-aside", { issues, layout: false });
     }
 
     default:
       break;
   }
-};
+}
 
 async function renderOneIssue(req, res) {
   const { id: issue_id } = req.params;
   const issue = await findOneIssue(issue_id);
   const tasks = await findTasksByIssueID(issue_id);
   // console.log(issue);
-  res.status(200).render('issue-ID', { issue, tasks, layout: false });
-};
+  res.status(200).render("issue-ID", { issue, tasks, layout: false });
+}
 
 async function renderIssuesByProperty(req, res) {
   const { id: property_id } = req.params;
   const issue = await getIssuesByPropertyID(property_id);
   console.log(issue);
-  res.status(200).render('issue-ID', { issue, layout: false });
-};
+  res.status(200).render("issue-ID", { issue, layout: false });
+}
 
 async function renderIsIssueDone(req, res) {
   const { id: issue_id } = req.params;
@@ -69,48 +81,43 @@ async function renderIsIssueDone(req, res) {
   const issue = await findOneIssue(issue_id);
   const tasks = await findTasksByIssueID(issue_id);
 
-  res.status(200).set("HX-Trigger", "update-aside").render("issue-ID", { issue, tasks, layout: false });
+  res
+    .status(200)
+    .set("HX-Trigger", "update-aside")
+    .render("issue-ID", { issue, tasks, layout: false });
 }
 
 async function renderNewIssue(req, res) {
-  const {
-    issue_title,
-    issue_description,
-    property_id
-  } = req.body;
+  const { issue_title, issue_description, property_id } = req.body;
 
   const issue = await createIssue({
     issue_title,
     issue_description,
-    property_id
+    property_id,
   });
 
-  res.status(200).json({ msg: 'Created', issue });
-};
+  res.status(200).json({ msg: "Created", issue });
+}
 
 async function renderUpdatedIssue(req, res) {
   const issue_id = req.params.id;
-  const {
-    issue_title,
-    issue_description,
-    property_id
-  } = req.body;
+  const { issue_title, issue_description, property_id } = req.body;
 
   const issue = await updateIssue(issue_id, {
     issue_title,
     issue_description,
-    property_id
+    property_id,
   });
 
   res.status(200).json({ msg: "Updated", issue });
-};
+}
 
 async function renderDeletedIssue(req, res) {
   const issue_id = req.params.id;
 
   const issue = await deleteIssue(issue_id);
   res.status(200).set("HX-Redirect", "/").end();
-};
+}
 
 async function renderAddVendor(req, res) {
   const issue_id = req.params.id;
@@ -118,7 +125,7 @@ async function renderAddVendor(req, res) {
 
   const result = await addVendorToIssue(issue_id, vendor_id);
   res.status(200).json({ msg: "Vendor added", result });
-};
+}
 
 async function renderIsTaskDone(req, res) {
   const { issue_id, task_id } = req.params;
@@ -134,7 +141,10 @@ async function renderIsTaskDone(req, res) {
   const issue = await findOneIssue(issue_id);
   const tasks = await findTasksByIssueID(issue_id);
 
-  res.status(200).set("HX-Trigger", "update-aside").render("issue-ID", { issue, tasks, layout: false });
+  res
+    .status(200)
+    .set("HX-Trigger", "update-aside")
+    .render("issue-ID", { issue, tasks, layout: false });
 }
 
 // async function findAllIssues() {
@@ -214,7 +224,6 @@ async function renderIsTaskDone(req, res) {
 //   return result;
 // }
 
-
 module.exports = {
   renderOpenIssues,
   renderAddVendor,
@@ -225,5 +234,5 @@ module.exports = {
   renderUpdatedIssue,
   renderIsTaskDone,
   renderIssuesByProperty,
-  renderIsIssueDone
-}
+  renderIsIssueDone,
+};
--- routes/htmx/taskHxRoutes.js
+++ routes/htmx/taskHxRoutes.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const c = require('../../controller/taskController');
 
 router.route('/')
-  .get(c.renderOpenTasks);
-  // .get(c.renderOneTask);
+  .get(c.renderOpenTasks)
+  // .get(c.renderOneTask)
+  .post(c.createTask);
 
 module.exports = router;
