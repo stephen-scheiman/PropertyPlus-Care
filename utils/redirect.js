5fe3f7d3387a842faf026a22ce0acd8f3122958c
Meow Roar <sareacct91@gmail.com>
2024-02-22 19:39:22 -0800

    change the redirect middleware to redirect instead of render

--- utils/redirect.js
+++ utils/redirect.js
@@ -4,7 +4,7 @@ function pageRedirect(req, res, next) {
   if (req.headers['hx-request']) {
     return next();
   }
-  return res.status(200).set('hx-request', true).render('homepage');
+  return res.status(200).set('hx-request', true).redirect('/');
 }
 
 module.exports = pageRedirect;
