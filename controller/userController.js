8c6f1a9a4fff791f84d6a67a9c6ff92cf5ce1eb1
Meow Roar <sareacct91@gmail.com>
2024-02-23 20:49:35 -0800

    added signup page

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,6 @@
 const { User } = require('../models');
 const { BadRequestError, NotFoundError } = require('../utils/errors');
-const { userLogin } = require('../utils/queries/users')
-const { findOpenTasks } = require('../utils/queries/tasks')
+const { userLogin, createUser } = require('../utils/queries/users')
 
 async function renderLoginForm(req, res) {
   // If the user is already logged in, redirect the request to another route
@@ -9,73 +8,49 @@ async function renderLoginForm(req, res) {
     res.redirect('/');
     return;
   }
-  res.render('login', {layout: 'main-login'});
+  res.status(200).render('login', {layout: 'main-login'});
 };
 
 async function renderLoggedInHome(req, res) {
   const { user_email, user_password } = req.body;
-  console.log(user_email, user_password, "\n\n");
-  const p1 = userLogin({user_email, user_password});
-  const p2 = findOpenTasks();
+  const user = await userLogin({ user_email, user_password });
 
-  const [user, taskData] = await Promise.all([p1, p2]);
-  console.log(user, taskData, "\n\n");
   req.session.save(() => {
     req.session.loggedIn = true;
     req.session.user_id = user.user_id;
 
-    res.status(200).render('homepage', {taskData})
+    res.status(200).set('hx-redirect', '/').end();
   })
 };
 
-async function createUser(req, res) {
-  const { user_name, user_email, user_password } = req.body;
+async function renderSignupForm(req, res) {
+  if (req.session.logged_in) {
+    return res.redirect('/');
+  }
+  res.status(200).render('signup', {layout: 'main-login'});
+}
+
+async function renderNewUser(req, res) {
+  const { user_name, user_password, user_email } = req.body;
 
   if (!(user_name && user_email && user_password)) {
     throw new BadRequestError('Missing Data - Please fill out all required fields.');
   }
 
-  const newUser = await User.create({
-    user_name,
-    user_email,
-    user_password
-  });
+  const user = await createUser({ user_name, user_password, user_email });
 
   req.session.save(() => {
     req.session.loggedIn = true;
-    req.session.user_id = newUser.user_id;
-    // if api send json IF htmx send html / render
-    res.status(200).json(newUser);
-  });
-};
+    req.session.user_id = user.user_id;
 
-async function userLogin1(req, res) {
-  const { user_email, user_password } = req.body;
-
-  const userData = await User.findOne({ where: {email: user_email}});
-
-  if (!userData) {
-    throw new BadRequestError("Incorrect email or password, please try again or register a new account");
-  }
-
-  const validPassword = await userData.checkPassword(user_password);
-
-  if (!validPassword) {
-    throw new BadRequestError("Incorrect email or password, please try again or register a new account");
-  }
-
-  req.session.save(() => {
-    req.session.user_id = userData.user_id;
-    req.session.logged_in = true;
-
-    res.status(200).json(userData);
+    res.status(201).set('hx-redirect', '/').end();
   })
-};
+}
 
-async function userLogout(req, res) {
+async function renderLoggedOut(req, res) {
   if (req.session.logged_in) {
     req.session.destroy(() => {
-      res.status(204).end();
+      res.status(204).set('hx-redirect', '/login').end();
     });
   } else {
     throw new BadRequestError("You are not logged in.");
@@ -83,9 +58,9 @@ async function userLogout(req, res) {
 };
 
 module.exports = {
-  createUser,
-  userLogin,
-  userLogout,
   renderLoginForm,
-  renderLoggedInHome
+  renderLoggedInHome,
+  renderSignupForm,
+  renderNewUser,
+  renderLoggedOut,
 };
