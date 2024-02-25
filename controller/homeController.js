4dfdc74c17ce968b4590d8394f7fc0e153df8c5c
Meow Roar <sareacct91@gmail.com>
2024-02-25 04:05:30 -0800

    fixed homepage oob

--- controller/homeController.js
+++ controller/homeController.js
@@ -12,7 +12,6 @@ const { findUserByPk } = require('../utils/queries/users');
 // homepage loads all current and past due Tasks
 async function renderHome(req, res) {
   const { user_id, logged_in } = req.session;
-  const isOob = true;
 
   // calling two promises but not awaiting them individually
   // 3rd line of code awaits both promises before continuing.
@@ -22,7 +21,7 @@ async function renderHome(req, res) {
   const [user, tasks] = await Promise.all([p1,p2]);
 
   // this needs to be completed when we know what the homepage will look like
-  res.status(200).render('task-aside', { tasks, user, isOob, });
+  res.status(200).render('task-aside', { tasks, user, });
 }
 
 async function renderAside(req, res) {
