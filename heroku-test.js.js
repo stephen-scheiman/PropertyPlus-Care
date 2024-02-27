f135eb59c0132ba19bf7c8117ebb9316b2cd4e45
Meow Roar <sareacct91@gmail.com>
2024-02-27 14:46:11 -0800

    new date test

--- heroku-test.js.js
+++ heroku-test.js.js
@@ -1,7 +1,12 @@
 async function herokuServerTest() {
   const today = new Date();
-  today.setHours(0, 0, 0, 0);
 
+  console.log('\ntoday', today);
+
+  today.setHours(0, 0, 0, 0);
   console.log('\nsetHours all 0\n', today);
+
+  today.setHours(-8, 0, 0, 0);
+  console.log('\nsetHours -8\n', today);
 }
 herokuServerTest();
